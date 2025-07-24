cfe7844704fc9fb785381eb6d87058dcba58653c
Brandon <brandoncq036@hotmail.com>
2025-07-23 20:41:23 -0500

    Cambiando

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def detect_intent(question: str, generos_disponibles: list) -> tuple:
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://front-chat-bot.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
